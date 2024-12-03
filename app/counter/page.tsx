dcfa1eebbd1179533d39da2d53c7bd1993b645fe
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2024-12-03 00:45:37 +0000

    counter

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -4,24 +4,14 @@ import React, { useEffect, useState } from 'react'
 
 export default function page() {
 
-    function inicializa() {
-        const countStored = '3'
-        return countStored? parseInt(countStored) : 0
-    }
-
     const [count, setCount] = useState(() => {
         const countStored = localStorage.getItem('count')
         return countStored? parseInt(countStored) : 0
     })
 
     useEffect( () => {
-        console.log(count)
         localStorage.setItem('count', `${count}`);
     }, [count]);
-
-    useEffect( () => {
-        console.log('preludio')
-    }, []); 
 
     return <>
         <p>Contador: {count}</p>
